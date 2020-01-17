d5123e94f4e058789c7f2d6f535c817467241f70
Etty <beltrami.ester@gmail.com>
2020-01-17 14:25:58 +0000

    Move logic from Command to ModelAdmin

--- backend/voting/admin.py
+++ backend/voting/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from voting.models import RankRequest, Vote
+from django.urls import reverse
+from django.utils.html import format_html
+from voting.models import RankRequest, RankSubmission, Vote
 
 
 @admin.register(Vote)
@@ -9,6 +11,63 @@ class VoteAdmin(admin.ModelAdmin):
     search_fields = ("submission", "user")
 
 
+@admin.register(RankSubmission)
+class RankSubmission(admin.ModelAdmin):
+    list_display = (
+        "absolute_rank",
+        "absolute_score",
+        "title",
+        "topic",
+        "topic_rank",
+        "topic_score",
+        "level",
+        "language",
+        "speaker",
+        "gender",
+    )
+    ordering = ("absolute_rank",)
+
+    def title(self, obj):  # pragma: no cover
+        return obj.submission.title
+
+    def topic(self, obj):  # pragma: no cover
+        return obj.submission.topic.name
+
+    def level(self, obj):  # pragma: no cover
+        return obj.submission.audience_level.name
+
+    def language(self, obj):  # pragma: no cover
+        emoji = {"it": "🇮🇹", "en": "🇬🇧"}
+        langs = [emoji[lang.code] for lang in obj.submission.languages.all()]
+        return " ".join(langs)
+
+    def speaker(self, obj):  # pragma: no cover
+        return (
+            obj.submission.speaker.full_name
+            or obj.submission.speaker.name
+            or obj.submission.speaker.email
+        )
+
+    def gender(self, obj):  # pragma: no cover
+        emoji = {
+            "": "",
+            "male": "👨🏻‍💻",
+            "female": "👩🏼‍💻",
+            "other": "🧑🏻‍🎤",
+            "not_say": "⛔️",
+        }
+        return emoji[obj.submission.speaker.gender]
+
+
 @admin.register(RankRequest)
 class RankRequestAdmin(admin.ModelAdmin):
-    list_display = ("conference", "created")
+    list_display = ("conference", "created", "view_rank")
+
+    def view_rank(self, obj):  # pragma: no cover
+        return format_html(
+            '<a class="button" href="{}">Open</a>&nbsp;',
+            reverse("admin:voting_ranksubmission_changelist"),
+        )
+
+    view_rank.short_description = "View"
+    view_rank.allow_tags = True
